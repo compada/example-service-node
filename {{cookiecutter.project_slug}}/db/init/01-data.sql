2e87fd2eb876ee92f6fb9e7b1a98532b61c2731a
Brett C. Dudo <brett@dudo.io>
2022-12-05 22:11:32 -0800

    space

--- {{cookiecutter.project_slug}}/db/init/01-data.sql
+++ {{cookiecutter.project_slug}}/db/init/01-data.sql
@@ -1,5 +1,5 @@
 \connect {{ cookiecutter.resource_plural }};
 
 INSERT INTO public.{{ cookiecutter.resource }} (id, attr1, attr2) VALUES
-('d67fa44a-70c1-4d1a-89ba-973f9abdfe3f', 'foo', 'bar'),
-('257bc9a6-ba9b-4608-820d-c818e4d391a7', 'baz', 'qux');
+  ('d67fa44a-70c1-4d1a-89ba-973f9abdfe3f', 'foo', 'bar'),
+  ('257bc9a6-ba9b-4608-820d-c818e4d391a7', 'baz', 'qux');
